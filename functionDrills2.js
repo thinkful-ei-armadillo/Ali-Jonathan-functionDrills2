c69b9d38657d36ef32d25dceafb63c1690069e88
Jonathan Kleinpeter <joklein341@gmail.com>
2019-01-14 16:08:48 -0600

    to infinity...

--- functionDrills2.js
+++ functionDrills2.js
@@ -5,3 +5,19 @@ function jediName(firstName, lastName) {
 }
 console.log(jediName("Abcd", "Efgh"));
 
+function beyond(num) {
+	if (num === Infinity) {
+		console.log('And beyond');
+	} else if (num > 0) {
+		console.log('To infinity');
+	} else if (num < 0) {
+		console.log('To negative infinity');
+	} else if (num === 0){
+		console.log('Staying home');
+	}
+}
+
+beyond(33);
+beyond(0);
+beyond(Infinity);
+beyond(-1);
